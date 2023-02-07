2c4022947a5ecc921057ce4fbc6f7095c1e62aab
sky-creations <121612750+sky-botz@users.noreply.github.com>
2023-02-07 13:14:23 +0530

    Update pm_filter.py

--- plugins/pm_filter.py
+++ plugins/pm_filter.py
@@ -62,7 +62,7 @@ async def give_filter(client, message):
 async def next_page(bot, query):
     ident, req, key, offset = query.data.split("_")
     if int(req) not in [query.from_user.id, 0]:
-        return await query.answer("oKda", show_alert=True)
+        return await query.answer("oKda", show_alert=False)
     try:
         offset = int(offset)
     except:
@@ -106,7 +106,7 @@ async def next_page(bot, query):
         ]
     btn.insert(0, 
         [
-            InlineKeyboardButton('📥 Hᴏᴡ ᴛᴏ ᴅᴏᴡɴʟᴏᴀᴅ 📥', url='https://t.me/shorturlink_tutorial')
+            InlineKeyboardButton('📥 Hᴏᴡ ᴛᴏ ᴅᴏᴡɴʟᴏᴀᴅ 📥', url='https://t.me/mdiskshortnerlink_tutorial')
         ]
     )
     btn.insert(1, 
